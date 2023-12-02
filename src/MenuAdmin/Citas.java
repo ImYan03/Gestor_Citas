738919c6f1d32bd17bc2f27fb170e1334058e6af
MrRaccoon03 <jeancmendozam13@gmail.com>
2023-12-02 17:24:11 -0500

    27

---
+++ src/MenuAdmin/Citas.java
@@ -0,0 +1,16 @@
+package MenuAdmin;
+
+import javax.swing.JPanel;
+
+public class Citas extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Citas() {
+
+	}
+
+}
---
+++ src/MenuAdmin/Config.java
@@ -0,0 +1,18 @@
+package MenuAdmin;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class Config extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Config() {
+		setBackground(new Color(64, 0, 64));
+
+	}
+
+}
